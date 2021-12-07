5dea1239c6a0011fe3b451c2546e1e335f192ed1
Wilson <wj.buena@gmail.com>
2021-12-07 20:17:00 +0800

    Removed variable

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+    const [blogs] = useState([
         { title: 'My New Website', body: 'This is my new website', author: 'Wilson', id: 1},
         { title: 'New Blog', body: 'This is a new blog', author: 'Jeffrey', id: 2},
         { title: 'New Blog 2', body: 'This is another blog', author: 'Buena', id: 3}
