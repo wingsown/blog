843ab17675a793bfaf42903df2dbf44c49957a8f
Wilson <wj.buena@gmail.com>
2021-12-07 19:58:50 +0800

    use state import

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My New Website', body: 'This is my new website', author: 'Wilson', id: 1},
+        { title: 'New Blog', body: 'This is a new blog', author: 'Jeffrey', id: 2},
+        { title: 'New Blog 2', body: 'This is another blog', author: 'Buena', id: 3}
+    ]);
+
+
     return (
         <div className="home">
             <h2>Homepage</h2>
