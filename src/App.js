35a76dd7794df49e25eee7d39cfaf0ce8cd9b25d
Wilson <wj.buena@gmail.com>
2021-12-07 11:06:15 +0800

    Import home to root component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="home">
-            
+            <h2>Homepage</h2>
         </div>
       );
 }
