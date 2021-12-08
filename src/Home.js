6bb5ad7da51a8c6769321dc13cc4b326cba898d0
Wilson <wj.buena@gmail.com>
2021-12-08 19:49:52 +0800

    handle delete function

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,18 @@ const Home = () => {
         { title: 'New Blog 2', body: 'This is another blog', author: 'Buena', id: 3}
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
 
     return (
         <div className="home">
            <BlogList
             blogs={blogs}
             title="All Blogs"
+            handleDelete={handleDelete}
            />
         </div>
       );
