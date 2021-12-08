d7ea1235fac163f6d23c9d74797261d890fd671c
Wilson <wj.buena@gmail.com>
2021-12-08 19:26:48 +0800

    New bloglist component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [blogs] = useState([
+    const [blogs, setBlogs] = useState([
         { title: 'My New Website', body: 'This is my new website', author: 'Wilson', id: 1},
         { title: 'New Blog', body: 'This is a new blog', author: 'Jeffrey', id: 2},
         { title: 'New Blog 2', body: 'This is another blog', author: 'Buena', id: 3}
@@ -10,12 +10,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by: { blog.author}</p>
-                </div>
-            ))}
+           
         </div>
       );
 }
