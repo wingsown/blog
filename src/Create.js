961fad9709e5b6906c9c1f6b5c2cdddbfafdae1f
Wilson <wj.buena@gmail.com>
2021-12-09 22:15:57 +0800

    Added ispengind state

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,13 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('wilson');
+    const [isPending, setIsPending] = useState(false);
     
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
+
+        setIsPending(true);
 
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
@@ -16,6 +19,7 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(() => {
             console.log('New blog added')
+            setIsPending(false);
         })
     }
 
@@ -42,7 +46,8 @@ const Create = () => {
                     <option value="wilson">Wilson</option>
                     <option value="pogi">Pogi</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button>Adding Blog...</button> }
             </form>
         </div>
       );
