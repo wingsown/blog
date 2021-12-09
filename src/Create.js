1096548242d3062db0e146e3b09baf5a42b48515
Wilson <wj.buena@gmail.com>
2021-12-09 22:22:48 +0800

    Use history hook

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('wilson');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
     
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,7 +22,10 @@ const Create = () => {
         }).then(() => {
             console.log('New blog added')
             setIsPending(false);
+            history.push('/');
         })
+
+
     }
 
     return (
