48a9fede0739fe56736b410d6285a3df2f666c3d
Wilson <wj.buena@gmail.com>
2021-12-09 19:52:01 +0800

    Transfer useeffect hook to usefetch custom hook

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Could not fetch data');
-            }
-
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-    }, []);
-
-
+  
     return (
         <div className="home">
             { error && <div>{ error }</div> }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,32 @@
+import { useState, useEffect } from 'react';
+
+
+
 const useFetch = () => {
-    
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Could not fetch data');
+            }
+
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, []);
 }
+
+export default useFetch;
