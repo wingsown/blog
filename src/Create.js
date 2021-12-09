740ea006c8236eda355f7f85c24d8e7e85da5a0e
Wilson <wj.buena@gmail.com>
2021-12-09 21:38:38 +0800

    Added input form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,17 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input type="text" required/>
+                <label>Blog:</label>
+                <textarea required></textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="wilson">Wilson</option>
+                    <option value="pogi">Pogi</option>
+                </select>
+            </form>
         </div>
       );
 }
