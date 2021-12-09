90c9881950a10026a5ad7a5c2ab596825deda353
Wilson <wj.buena@gmail.com>
2021-12-09 10:21:30 +0800

    Fetch data from fake server

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,22 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My New Website', body: 'This is my new website', author: 'Wilson', id: 1},
-        { title: 'New Blog', body: 'This is a new blog', author: 'Jeffrey', id: 2},
-        { title: 'New Blog 2', body: 'This is another blog', author: 'Buena', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    useEffect();
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
 
 
     return (
