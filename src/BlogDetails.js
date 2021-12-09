63e72e93446cefa729224de783e6fc7e117fa741
Wilson <wj.buena@gmail.com>
2021-12-09 20:55:01 +0800

    Route params

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router";
+
 const BlogDetails = () => {
+    const { id } = useParams();
+
+
     return (  
         <div className="blog-details">
-            <h2>Blog details</h2>
+            <h2>Blog details - { id }</h2>
         </div>
     );
 }
