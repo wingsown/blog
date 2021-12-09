dd12cf4f71c16d4c45fc4d4b6116acfb7dda32e4
Wilson <wj.buena@gmail.com>
2021-12-09 21:55:19 +0800

    Use state on input fields

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog Title:</label>
-                <input type="text" required/>
+                <input 
+                type="text" 
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
                 <label>Blog:</label>
                 <textarea required></textarea>
                 <label>Blog Author:</label>
