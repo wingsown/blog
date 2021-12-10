eddbc24c44f46937a6be421b1dbc9b31418ecb84
Wilson <wj.buena@gmail.com>
2021-12-10 21:20:55 +0800

    Add blog event handler

--- src/Create.js
+++ src/Create.js
@@ -1,42 +1,59 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from './firebase-config';
 
 const Create = () => {
 
+    const blogsRef = collection(db, "blogs");
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = { title, body, author };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const blog = { title, body, author };
 
+    //     setIsPending(true);
+
+    //     fetch('http://localhost:8000/blogs', {
+    //         method: 'POST',
+    //         headers: { "Content-Type": "application/json" },
+    //         body: JSON.stringify(blog)
+    //     }).then(() => {
+    //         console.log('New blog added')
+    //         setIsPending(false);
+    //         history.push('/');
+    //     })
+
+
+    // }
+
+    const addNewBlog = async (e) => {
+        e.preventDefault();
+        
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(blog)
+        await addDoc(blogsRef, {
+            title: title,
+            body: body,
+            author: author
         }).then(() => {
-            console.log('New blog added')
+            console.log('New blog added');
             setIsPending(false);
-            history.push('/');
         })
-
-
     }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <label>Blog Title:</label>
                 <input 
                 type="text" 
                 required
-                value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog:</label>
@@ -48,10 +65,9 @@ const Create = () => {
                 <input 
                 type="text" 
                 required
-                value={author}
                 onChange={(e) => setAuthor(e.target.value)}
                 />
-                { !isPending && <button>Add Blog</button> }
+                { !isPending && <button onClick={addNewBlog}>Add Blog</button> }
                 { isPending && <button disabled>Adding Blog...</button> }
             </form>
         </div>
