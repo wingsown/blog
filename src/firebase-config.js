4718dd16c21426237dec0a50f747446b5f1ec54c
Wilson <wj.buena@gmail.com>
2021-12-10 19:52:08 +0800

    Configuration for firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvVFkDXqlSx7kdTx4KOqlbEz46l645vTY",
+    authDomain: "blog-a3fbd.firebaseapp.com",
+    projectId: "blog-a3fbd",
+    storageBucket: "blog-a3fbd.appspot.com",
+    messagingSenderId: "262012698092",
+    appId: "1:262012698092:web:92e875056b1cb0034994c9",
+    measurementId: "${config.measurementId}"
+  };
+
+  const app = initializeApp(firebaseConfig);
